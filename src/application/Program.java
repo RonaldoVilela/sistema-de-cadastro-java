c26b5c8cf37b88faf18f6135a736bfcf3f251456
Ronaldo Vilela <ronalldo.vilela@gmail.com>
2024-04-24 13:37:44 -0300

    Lógica principal

---
+++ src/application/Program.java
@@ -0,0 +1,225 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+
+import user.Account;
+
+public class Program {
+	public static Set<Account> accounts = new HashSet<>();
+	static Account contaAtual = null;
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		carregarContas();
+		int codigo = 0;
+		
+		while(codigo != 3) {
+			if(contaAtual != null) {
+				acessarPerfil();
+			}
+			
+			System.out.println("=== MENU ===");
+			System.out.println("1 - Criar nova conta");
+			System.out.println("2 - Fazer login");
+			System.out.println("3 - Sair \n");
+			System.out.print("Digite o código: ");
+			
+			try {
+				codigo = sc.nextInt();
+
+				System.out.println("-------------- \n");
+				
+				switch (codigo) {
+				case 1:cadastrar();
+					break;
+				case 2: login();
+					break;
+				case 3: break;
+	
+				default:
+					System.out.println("!Código Inválido! \n");
+					pause(1000);
+					break;
+				}
+			}catch(InputMismatchException e) {
+
+				System.out.println("!Código Inválido! \n");
+				sc.nextLine();
+				codigo = 0;
+				pause(1000);
+			}
+			//limparTela();
+		}
+		sc.close();
+	}
+	
+	static void cadastrar() {
+		System.out.println("=== CADASTRO ===");
+		System.out.println("(digite 'q' a qualquer momento para cancelar) \n");
+		
+		String nome = null;
+
+		sc.nextLine();
+		do {
+			System.out.print("Nome: ");
+
+			nome = sc.nextLine();
+			
+			if(nome.equals("q")) {
+				return;
+			}
+		}while(!Account.nomeValido(nome));
+		
+		String email = null;
+		
+		do {
+			System.out.print("E-mail: ");
+			email = sc.next();
+			
+			if(email.equals("q")) {
+				return;
+			}
+		}while(!Account.emailValido(email));
+		
+		String senha = null;
+		
+		do {
+			System.out.print("Senha: ");
+			senha = sc.next();
+			
+			if(senha.equals("q")) {
+				return;
+			}
+		}while(!Account.senhaValida(senha));
+		
+		accounts.add(new Account(nome, email, senha));
+		salvarContas();
+		System.out.println("*Conta salva com sucesso*");
+		pause(1000);
+		
+	}
+	
+	static void login() {
+		if(accounts.size() == 0) {
+			System.out.println("Você ainda não tem nenhuma conta salva!");
+			pause(1000);
+			return;
+		}
+		
+		System.out.println("=== LOGIN ===");
+		System.out.println("(digite 'q' a qualquer momento para cancelar) \n");
+		
+		Account conta = null;
+		do {
+			System.out.print("E-mail: ");
+			
+			String email = null;
+			email = sc.next();
+			
+			if(email.equals("q")) {
+				return;
+			}
+				
+			for(Account a: accounts) {
+				if(a.getEmail().equals(email)) {
+					conta = a;
+				}
+			}
+			if(conta == null) {
+				System.out.println("E-mail incorreto ou inexistente!");
+				pause(1000);
+			}
+		}
+		while(conta == null);
+		
+
+		System.out.println("Você está entrando na conta de: "+conta.getName());
+		String senha = null;
+		do {
+			System.out.print("Senha: ");
+			senha = sc.next();
+			if(senha.equals("q")) {
+				return;
+			}
+			
+		}while(!conta.verificarSenha(senha));
+		contaAtual = conta;
+		
+	}
+	
+	static void acessarPerfil() {
+		int codigo = 0;
+		do {
+			System.out.println("\n === PERFIL === \n");
+			System.out.println("Olá, "+ contaAtual.getName()+"!");
+			System.out.println(contaAtual.getEmail());
+			System.out.println("-----------------\n");
+			pause(1000);
+			System.out.println("1 - Excluir conta");
+			System.out.println("2 - Sair da conta \n");
+			
+			System.out.print("Digite o código: ");
+			codigo = sc.nextInt();
+			
+			if(codigo == 1) {
+				excluirContaAtual();
+				return;
+			}else if(codigo != 2){
+				System.out.println("Código Invalido!");
+			}
+			
+		}while(codigo != 2);
+	}
+	
+	public static void pause(long mills) {
+		try {
+			Thread.sleep(mills);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void salvarContas() {
+		File contas = new File("res\\contas.txt");
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(contas))){
+			for(Account a: accounts) {
+				bw.write(a.getName()+";"+a.getEmail()+";"+a.getPassword());
+				bw.newLine();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void carregarContas() {
+		File contas = new File("res\\contas.txt");
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(contas))){
+			String linha = br.readLine();
+			while(linha != null) {
+				String[] info = linha.split(";");
+				accounts.add(new Account(info[0], info[1], info[2]));
+				linha = br.readLine();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void excluirContaAtual() {
+		accounts.remove(contaAtual);
+		contaAtual = null;
+		salvarContas();
+	}
+
+}
---
+++ src/user/Account.java
@@ -0,0 +1,92 @@
+package user;
+
+
+import application.Program;
+
+public class Account{
+	
+	private String name;
+	private String email;
+	private String password;
+	
+	
+	public Account() {}
+	public Account(String name, String email, String pasword) {
+		this.name = name;
+		this.email = email;
+		this.password = pasword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPasword(String pasword) {
+		this.password = pasword;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public static boolean nomeValido(String nome) {
+		if(nome.contains(";") || nome.contains(":")) {
+			System.out.println("!No nome, não é permitido o uso de caracteres especiais como: [;:!?.*//\\]!");
+			Program.pause(1000);
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean emailValido(String email) {
+		if(email.contains(" ") || email.contains(";")) {
+			System.out.println("!No email, não é permitido o uso de espaço ou caracteres especiais como: [;:!.*//\\]!");
+			Program.pause(1000);
+			return false;
+		}
+		if(!email.contains("@")) {
+			System.out.println("E-mail invalido!");
+			Program.pause(1000);
+			return false;
+		}
+		
+		for(Account a: Program.accounts) {
+			if(a.getEmail().equals(email)) {
+				System.out.println("E-mail já existente!");
+				Program.pause(1000);
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean senhaValida(String senha) {
+		if(senha.contains(" ") || senha.contains(";")) {
+			System.out.println("!Na senha, não é permitido o uso de espaço ou caracteres especiais como: [;:!.*//\\]!");
+			Program.pause(1000);
+			return false;
+		}
+		if(senha.length() < 4) {
+			System.out.println("A senha deve ter pelo menos 4 caracteres!");
+			Program.pause(1000);
+			return false;
+		}
+		return true;
+	}
+	
+	
+	public boolean verificarSenha(String senha) {
+		if(password.equals(senha)) {
+			return true;
+		}
+		System.out.println("Senha Incorreta!");
+		Program.pause(1000);
+		return false;
+	}
+}
